b23bcb68ac60774b571873598a7a161c5b6f93bd
Spencer Reitman Desktop <spencerreitman@gmail.com>
2017-05-31 20:55:41 -0700

    changed code.c

--- code.c
+++ code.c
@@ -1,12 +1,14 @@
 # include "code.h"
 
+# define INCREMENT 8000
 void appendCode(code C, bitV *bv)
 {
 	static uint32_t position = 0;
-	while ((bv->l)+C->l > bv->l)
+	while ((bv->l) + C->l > bv->l)
 	{
-		realloc(bv, (bv->l)+1000);
+		bv->l += INCREMENT;
+		realloc(bv->vector, bv->l*(sizeof(uint8_t)));
 	}
-	bv->v[position] |= code->bits[0];
+	bv->vector[position] |= code->bits[0];
 	position += C->l;
 }
