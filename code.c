162d4caf2e0c854511a224ad0647bbca6ee8ccd0
Gabriel Torres <jhd389@gmail.com>
2017-06-07 22:00:02 -0700

    Last comments done. Project is now done.

--- code.c
+++ code.c
@@ -7,13 +7,13 @@
 
 uint32_t appendCode(code C, bitV *bv)
 {
-	static uint32_t position = 0;
-	// printf("Before Appending code\n");
-	// printVec(bv);
+	static uint32_t position = 0; // keeps track of where to insert bits
+	// reallocs if the bit vector is too small
 	while(position + C.l > bv->length)
 	{
 		bv->length += INCREMENT;
 		uint8_t *temp = (uint8_t *) realloc(bv->vector, bv->length * (sizeof(uint8_t)));
+		// error handling
 		if(temp == NULL)
 		{
 			printf("ERROR IN REALLOC\n");
@@ -23,22 +23,22 @@ uint32_t appendCode(code C, bitV *bv)
 		{
 			bv->vector = temp;
 		}
+		// realloc does not zero out the new memory allocated so we must do it ourselves
 		for (uint32_t i = position; i < bv->length; i++)
 	  {
 	    clrBit(bv, i);
 	  }
-		// printf("\nafter realloc\n");
-		// printVec(bv);
 	}
+	// appends the code to the bitVector
 	for (uint32_t i = position; (i - position) < C.l; i++)
 	{
+		// if the bit is a 1, set the bit in the bit vector
 		if (C.bits[(i - position)/8] & (0x1 << ((i - position) % 8)))
 		{
 			setBit(bv, i);
 		}
 	}
-	// printf("\nafter appendCode\n");
-	// printVec(bv);
+	// updates the position
 	position += C.l;
 	return position;
 }
